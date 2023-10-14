c66b3466b77d75ff36f8a882b73a3890896ca07c
Ashfaq Hussain Ahmed <ashfaqshuvo14@gmail.com>
2023-10-14 10:59:28 +0300

    Initial Setup

---
+++ src/test/kotlin/com/personal/taskmnanagerapi/TaskMnanagerApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.personal.taskmnanagerapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskMnanagerApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
